b580c87655a5d1177f5aa37795630397e48361ef
nguyenl3v <nguyenka1358@gmail.com>
2019-10-16 11:52:04 +0700

    config server.js

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.use("/admin/menu", adminMenu);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client"));
   app.get("*", function(req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "index.js"));
   });
 }
 
