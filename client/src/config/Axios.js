938f7553c2bfbe6d88c685a5b2fcec1781656d18
nguyenl3v <nguyenka1358@gmail.com>
2019-10-16 11:18:37 +0700

    fix cors

--- client/src/config/Axios.js
+++ client/src/config/Axios.js
@@ -1,1 +1,4 @@
-export const API = "http://localhost:4000";
+export const API =
+  process.env.NODE_ENV === "production"
+    ? "https://shielded-beyond-24124.herokuapp.com"
+    : "http://localhost:4000";
